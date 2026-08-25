70b190ea8721dd8d3abb8f23fbae416b6b0af147
Enzo <enzo@example.com>
2026-08-24 21:43:56 -0300

    fix: substituir V9 falha pela versao segura com update de fk

--- ProdSync.api/src/main/resources/db/migration/V9__clean_duplicates.sql
+++
@@ -1,7 +0,0 @@
-﻿DELETE c1 FROM colaboradores c1
-INNER JOIN colaboradores c2 
-WHERE c1.id > c2.id AND c1.nome = c2.nome;
-
-DELETE t1 FROM tipos_atividade t1
-INNER JOIN tipos_atividade t2
-WHERE t1.id > t2.id AND t1.nome = t2.nome;
